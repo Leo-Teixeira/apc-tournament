99ce53b3399dbd2347c29b32d663776b4264dade
Leo-Teixeira <teixeiraleo72@gmail.com>
2025-07-21 20:56:18 +0200

    setup project in windows

--- src/middleware.ts
+++ src/middleware.ts
@@ -10,47 +10,47 @@ export async function middleware(request: NextRequest) {
   const alreadyValidated =
     request.cookies.get("admin_token_validated")?.value === "true";
 
-  if (!token) {
-    if (!alreadyValidated) {
-      return NextResponse.redirect(new URL("/error/unauthorized", request.url));
-    }
-    return NextResponse.next();
-  }
+  // if (!token) {
+  //   if (!alreadyValidated) {
+  //     return NextResponse.redirect(new URL("/error/unauthorized", request.url));
+  //   }
+  //   return NextResponse.next();
+  // }
 
-  if (!alreadyValidated) {
-    try {
-      const res = await fetch(
-        "https://aqua-bat-544144.hostingersite.com/wp-json/custom/validate-token",
-        {
-          headers: { Authorization: `Bearer ${token}` }
-        }
-      );
+  // if (!alreadyValidated) {
+  //   try {
+  //     const res = await fetch(
+  //       "https://aqua-bat-544144.hostingersite.com/wp-json/custom/validate-token",
+  //       {
+  //         headers: { Authorization: `Bearer ${token}` }
+  //       }
+  //     );
 
-      if (!res.ok) {
-        console.log("Token rejected, redirecting to unauthorized.");
-        return NextResponse.redirect(
-          new URL("/error/unauthorized", request.url)
-        );
-      }
+  //     if (!res.ok) {
+  //       console.log("Token rejected, redirecting to unauthorized.");
+  //       return NextResponse.redirect(
+  //         new URL("/error/unauthorized", request.url)
+  //       );
+  //     }
 
-      console.log("Token validated successfully.");
-      const response = NextResponse.next();
-      response.cookies.set("admin_token", token, {
-        httpOnly: true,
-        secure: true,
-        path: "/"
-      });
-      response.cookies.set("admin_token_validated", "true", {
-        path: "/",
-        secure: true,
-        maxAge: 60 * 60
-      });
-      return response;
-    } catch (err) {
-      console.error("Token validation failed:", err);
-      return NextResponse.redirect(new URL("/error/unauthorized", request.url));
-    }
-  }
+  //     console.log("Token validated successfully.");
+  //     const response = NextResponse.next();
+  //     response.cookies.set("admin_token", token, {
+  //       httpOnly: true,
+  //       secure: true,
+  //       path: "/"
+  //     });
+  //     response.cookies.set("admin_token_validated", "true", {
+  //       path: "/",
+  //       secure: true,
+  //       maxAge: 60 * 60
+  //     });
+  //     return response;
+  //   } catch (err) {
+  //     console.error("Token validation failed:", err);
+  //     return NextResponse.redirect(new URL("/error/unauthorized", request.url));
+  //   }
+  // }
 
   return NextResponse.next();
 }
