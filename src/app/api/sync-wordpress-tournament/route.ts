418873b7064851de8b4525c0ef37eee5ea644d08
Leo-Teixeira <teixeiraleo72@gmail.com>
2025-08-20 08:57:08 +0200

    :sparkles: change post request wordpress

--- src/app/api/sync-wordpress-tournament/route.ts
+++ src/app/api/sync-wordpress-tournament/route.ts
@@ -29,15 +29,42 @@ export async function POST(req: NextRequest) {
       tournament_stack = 1
     } = body;
 
+    const trimesterNumberMatch = tournament_trimestry.match(/^T(\d)$/);
+    if (!trimesterNumberMatch) {
+      return NextResponse.json({ error: "Invalid trimester format" }, { status: 400 });
+    }
+    const trimesterNumber = parseInt(trimesterNumberMatch[1], 10);
+
+    const currentSeason = await prisma.season.findFirst({
+      where: {
+        status: 'in_progress',
+      }
+    });
+
+    if (!currentSeason) {
+      return NextResponse.json({ error: "Current season not found" }, { status: 400 });
+    }
+
+    const trimester = await prisma.trimester.findFirst({
+      where: {
+        number: trimesterNumber,
+        season_id: currentSeason.id,
+      }
+    });
+
+    if (!trimester) {
+      return NextResponse.json({ error: "Trimester not found for current season" }, { status: 400 });
+    }
+
     const createdTournament = await prisma.tournament.create({
       data: {
-        wordpress_post_id: wordpress_post_id, // <= ici
+        wordpress_post_id,
         tournament_name,
         tournament_description,
         tournament_start_date: new Date(tournament_start_date),
         tournament_open_date: new Date(tournament_open_date),
         estimate_duration: parseTimeToDateObject(estimate_duration),
-        tournament_trimestry,
+        tournament_trimestry: trimester.id,
         tournament_category,
         tournament_status,
         tournament_stack
@@ -45,11 +72,10 @@ export async function POST(req: NextRequest) {
     });
 
     return NextResponse.json(serializeBigInt(createdTournament));
+
   } catch (err) {
     console.error("Error creating tournament from WordPress:", err);
-    return NextResponse.json(
-      { error: "Failed to sync tournament" },
-      { status: 500 }
-    );
+    return NextResponse.json({ error: "Failed to sync tournament" }, { status: 500 });
   }
 }
+
