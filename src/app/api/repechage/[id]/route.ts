e4f41af8df02e3dcbcecc1d881fa4fe2905d1ce3
Leo-Teixeira <teixeiraleo72@gmail.com>
2025-08-21 18:52:35 +0200

    up

--- src/app/api/repechage/[id]/route.ts
+++ src/app/api/repechage/[id]/route.ts
@@ -13,7 +13,7 @@ export async function OPTIONS() {
 
 export async function DELETE(
   req: NextRequest,
-  { params }: { params: { id: string } }
+  { params }: { params: Promise<{ id: string }> }
 ) {
   try {
     const { id } = await params; // Ne pas await ici
