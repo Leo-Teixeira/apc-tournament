5857ef6bd1816d9f5b53c4eb3e9d396bd86a4153
Leo-Teixeira <teixeiraleo72@gmail.com>
2025-08-30 14:59:50 +0200

    up

--- src/app/(withSidebar)/layout.tsx
+++ src/app/(withSidebar)/layout.tsx
@@ -9,17 +9,15 @@ export default function CommonLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="h-full w-full overflow-hidden">
-      <div className="flex h-screen w-screen overflow-hidden">
+    <div className="h-full w-full overflow-auto">
+      <div className="flex min-h-screen w-screen overflow-visible">
         <div className="hidden md:block">
           <Sidebar />
         </div>
         <Divider orientation="vertical" className="border-2 hidden md:block" />
-        <div className="flex-1 min-w-0 min-h-0 overflow-y-auto p-0 bg-apt-gradient flex flex-col">
+        <div className="flex-1 min-w-0 min-h-0 overflow-y-auto p-0 bg-apt-gradient flex flex-col pb-safe-area">
           <div className="p-2 md:p-6">
-            <NotificationProvider>
-              {children}
-            </NotificationProvider>
+            <NotificationProvider>{children}</NotificationProvider>
           </div>
         </div>
       </div>
