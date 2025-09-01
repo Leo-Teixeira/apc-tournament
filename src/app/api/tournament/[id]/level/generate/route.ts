412f019a75a9ec02cad2e5857e885f6b07de611e
Leo-Teixeira <teixeiraleo72@gmail.com>
2025-09-01 19:02:09 +0200

    up

--- src/app/api/tournament/[id]/level/generate/route.ts
+++ src/app/api/tournament/[id]/level/generate/route.ts
@@ -84,7 +84,7 @@ export async function POST(req: NextRequest) {
         consecutivePlayedLevels = 0;
       } else {
         sb = bb;
-        bb += bb;
+        bb += 50;
         consecutivePlayedLevels++;
       }
 
